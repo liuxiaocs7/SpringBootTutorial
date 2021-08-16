4d0304dcbd9c67a29afd868f63a3b6495efc246f
liuxiao <liuxiao2103@qq.com>
2021-08-16 21:23:01 +0800

    测试事务

--- springboot_demo3/src/main/java/com/liuxiaocs/service/UserServiceImpl.java
+++ springboot_demo3/src/main/java/com/liuxiaocs/service/UserServiceImpl.java
@@ -27,6 +27,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public void save(User user) {
         userDAO.save(user);
+        // 测试事务
         int n = 1 / 0;
     }
 }
