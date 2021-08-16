825cd2f329a27ca9137097b999501cf1bc7c9947
liuxiao <liuxiao2103@qq.com>
2021-08-16 20:48:26 +0800

    整合DevTools

---
+++ springboot_demo3/src/main/java/com/liuxiaocs/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.liuxiaocs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("demo")
+    public String demo() {
+        System.out.println("demo hello");
+        return "demo hello";
+    }
+}
