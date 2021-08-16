f0a7d87e134cd5fb752fab0f4df50d1a8d5289e1
liuxiao <liuxiao2103@qq.com>
2021-08-16 13:38:17 +0800

    使用@Configuration和@Bean注解创建多个对象

--- springboot_demo2/src/main/java/com/liuxiaocs/controller/DemoController.java
+++ springboot_demo2/src/main/java/com/liuxiaocs/controller/DemoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Calendar;
+
 /**
  * 用来测试对象创建
  */
@@ -16,10 +18,22 @@ public class DemoController {
     @Qualifier(value = "demoService")  // 作用：用来修改autowired默认不再根据类型注入 修改为根据名称注入
     private DemoService demoService;
 
+    // 直接注入
+    @Autowired
+    @Qualifier("calendar")
+    private Calendar calendar;
+
     @RequestMapping("demo")
     public String demo() {
         System.out.println("demo ok");
         demoService.demo();
+
+        // 日历类，无法添加@Component注解，通过配置的方式创建出来
+        // Calendar instance = Calendar.getInstance();
+        // System.out.println("当前日期：" + instance.getTime());
+
+        System.out.println("工厂中获取日历对象，当前时间为：" + calendar.getTime());
+
         return "demo ok";
     }
 }
